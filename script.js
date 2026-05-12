6da91011f81281e69c3fc69d1b9f1ba843cd00d1
Robert Mudzimu <RobertMudzimuJr@gmail.com>
2026-05-12 08:39:42 +0200

    added geolocation feature  and display map using leaflet library

--- script.js
+++ script.js
@@ -10,3 +10,36 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      const coords = [latitude, longitude];
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const map = L.map('map').setView(coords, 16);
+
+      L.tileLayer(
+        `https://api.maptiler.com/maps/streets-v4/{z}/{x}/{y}.png?key=${'sHKaCXOCc2CTgH2DZtvH'}`,
+        {
+          //style URL
+          tileSize: 512,
+          zoomOffset: -1,
+          minZoom: 1,
+          attribution:
+            '\u003ca href="https://www.maptiler.com/copyright/" target="_blank"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href="https://www.openstreetmap.org/copyright" target="_blank"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e',
+          crossOrigin: true,
+        },
+      ).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('Could not get your location');
+    },
+  );
+}
