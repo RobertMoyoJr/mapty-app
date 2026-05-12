d1531def3166cde3ea49115c6f7fab11d9d33ee7
Robert Mudzimu <RobertMudzimuJr@gmail.com>
2026-05-12 11:58:48 +0200

    implemented ability to show marker on map

--- script.js
+++ script.js
@@ -15,8 +15,7 @@ if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude, longitude } = position.coords;
-      const coords = [latitude, longitude];
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      const coords = [37.0475008, -7.8381056];
 
       const map = L.map('map').setView(coords, 16);
 
@@ -33,10 +32,23 @@ if (navigator.geolocation) {
         },
       ).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            }),
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your location');
